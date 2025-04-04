ea9a856e4fc4594a5bf633e75ee6813daa7b72de
jihyeog144 <jiheog144@gmail.com>
2025-04-04 15:16:51 +0900

    초기 커밋.

---
+++ designPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace designPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
